97416764e231e5c21ad3e2ca6bde5fde5f3d84e4
Cary Coutant <ccoutant@gmail.com>
2014-09-01 10:29:33 -0700

    Add 404 text.

--- uploads.py
+++ uploads.py
@@ -16,6 +16,12 @@ class ServeFileHandler(blobstore_handlers.BlobstoreDownloadHandler):
 			self.send_blob(item.blob)
 		else:
 			self.error(404)
+			self.response.out.write('<html><head>\n')
+			self.response.out.write('<title>404 Not Found</title>\n')
+			self.response.out.write('</head><body>\n')
+			self.response.out.write('<h1>Not Found</h1>\n')
+			self.response.out.write('<p>The requested page "%s" was not found on this server.</p>\n' % name)
+			self.response.out.write('</body></html>\n')
 
 app = webapp2.WSGIApplication([('(/photos/.*)', ServeFileHandler),
 							   ('(/files/.*)', ServeFileHandler)],
