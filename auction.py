8f6b9170615d12474e036c512f354b5e65e95ccd
Cary Coutant <ccoutant@gmail.com>
2014-09-01 10:31:58 -0700

    Use dinner date from tournament properties.

--- auction.py
+++ auction.py
@@ -8,16 +8,21 @@ from django.template.loader import render_to_string
 
 import auctionitem
 import capabilities
+import tournament
 
 server_software = os.environ.get('SERVER_SOFTWARE')
 dev_server = True if server_software and server_software.startswith("Development") else False
 
 class Auction(webapp2.RequestHandler):
 	def get(self):
+		t = tournament.get_tournament()
+		dinner_date = "%s, %s %d, %d" % (t.dinner_date.strftime("%A"), t.dinner_date.strftime("%B"),
+										 t.dinner_date.day, t.dinner_date.year)
 		auction_items = auctionitem.get_auction_items()
 		caps = capabilities.get_current_user_caps()
 		template_values = {
 			'auction_items': auction_items,
+			'dinner_date': dinner_date,
 			'capabilities': caps
 			}
 		self.response.out.write(render_to_string('auction.html', template_values))
